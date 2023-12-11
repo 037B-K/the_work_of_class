a6d679d77a2e3f4719054c720a0e38f142fb6650
Léo Bertrand-Kunieda <bertrandkuniedal@educ.college-montreal.qc.ca>
2023-12-11 14:56:02 -0500

    Initial commiting suicide

---
+++ 7testung.py
@@ -0,0 +1,34 @@
+import random
+
+class NPC:
+    def __init__(self, name, job, race, espece):
+        self.force = random.randint(1, 6) + random.randint(1, 6) + random.randint(1, 6)
+        self.agilite = random.randint(1, 6) + random.randint(1, 6) + random.randint(1, 6)
+        self.constitution = random.randint(1, 6) + random.randint(1, 6) + random.randint(1, 6)
+        self.intelligence = random.randint(1, 6) + random.randint(1, 6) + random.randint(1, 6)
+        self.sagesse = random.randint(1, 6) + random.randint(1, 6) + random.randint(1, 6)
+        self.charisme = random.randint(1, 6) + random.randint(1, 6) + random.randint(1, 6)
+        self.armor = random.randint(1, 12)
+        self.pt_de_vie = random.randint(1, 20)
+        self.nom = name
+        self.job = job
+        self.race = race
+        self.espece = espece
+
+    def show_stats(self):
+        print("Force: ", self.force, "\nAgilite: ", self.agilite, "\nConstitution: ", self.constitution, "\nIntelligence: ",
+              self.intelligence, "\nSagesse: ", self.sagesse, "\nCharisme: ", self.charisme, "\nClasse d'armure: ", self.armor,
+              "\nPoints de vie: ", self.pt_de_vie, "\nNom: ", self.nom, "\nProfession: ", self.job, "\nRace: ", self.race,
+              "\nEspece: ", self.espece)
+
+
+
+class Kobold(NPC):
+    def __init__(self):
+        super.__init__(self)
+        pass
+    def attack(self, person):
+        pass
+
+
+
